618ff3de05445c6f943ccfb31f4fc90fe8e5f735
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-09-26 22:34:16 -0500

    Within the index html I added an a href to link each superhero to their details page. I created the creat html so that website users may add new superheroes to the database. Within the urls python file I added a new path to connect the create new superheroes page to the app. Within the views python file I added some imports from django http and django urls - HttpResponse and HttpResponseRedirect and reverse. Also within the views python file I created a create function that allows users to create new superheroes.

--- superhero_python_django/superheroes/views.py
+++ superhero_python_django/superheroes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
 
 # Create your views here.
@@ -16,4 +17,18 @@ def detail(request, hero_id):
         'single_hero': single_hero
     }
     return render(request, 'superheroes/detail.html', context)
-    
+
+def create(request):
+    if request.method == "POST":
+        #save the form contents as a new db object
+        #return to index
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
